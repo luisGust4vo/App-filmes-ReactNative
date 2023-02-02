811ec4df4f75e853ed1232dd181ad67cbe9a01cd
Luis Gustavo <75265319+luisGust4vo@users.noreply.github.com>
2023-02-01 22:37:53 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,38 +1,41 @@
-import React, { useEffect, useState} from 'react';
-import { View, Text, StyleSheet, FlatList} from 'react-native';
+import React, {useEffect,useState} from 'react';
+import { View,Text,StyleSheet,FlatList, ActivityIndicator } from 'react-native';
+import api from './src/services/api'
+import Filmes from './src/Filmes'
 
-import api from './src/services/api';
-import Filmes from './src/Filmes';
-
-export default function App() {
-  const [filmes, setFilmes] = useState([]);
-
-  useEffect(()=> {
-
+export default function App(){
+  const [filmes,setFilmes] = useState([]);
+  const [loading,setloading] = useState(true);
+  useEffect(()=>{
     async function loadFilmes(){
       const response = await api.get('r-api/?api=filmes');
       // console.log(response.data);
       setFilmes(response.data);
+      setloading(false);
     }
-
     loadFilmes();
+  },[]);
 
-  }, []);
-  
+  if(loading){
+    return(
+      <View style={{alignItems:'center',justifyContent:'center',flex:1}}>
+        <ActivityIndicator color={'#121212'} size={45}/>
+      </View>
+      
+      )
+  }
 
- return (
-   <View style={styles.container}>
-     
-     <FlatList
-     data={filmes}
-     keyExtractor={ item => String(item.id) }
-     renderItem={ ({ item }) => <Filmes data={item} /> }
-     />
+  return(
+    <View style={styles.container}>
+      <FlatList
+        data={filmes}
+        keyExtractor={item =>String(item.id)}
+        renderItem={({item})=><Filmes data={item} />}
+      />
+    </View>
 
-   </View>
-  );
+);
 }
-
 const styles = StyleSheet.create({
   container:{
     flex:1,
